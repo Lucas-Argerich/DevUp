d3d02e937eb9668473a328ed56eef67306623d5d
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-10-30 22:04:46 -0300

    Home & HomeLogo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import HomeLogo from "../../assets/icons/HomeLogo";
+import Button from "../../components/Button";
+
+const Wrapper = styled.div`
+  margin: 80px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Text = styled.span`
+  font-weight: 700;
+  text-align: center;
+`;
+
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <Wrapper>
+        <HomeLogo />
+        <Text>
+          The free-space for developers to share their endeavors <br /> and
+          interact with each others!
+        </Text>
+      </Wrapper>
+      <Button to="/">Sign Up</Button>
+      <Button to="/" secondary>
+        Sign In
+      </Button>
+    </>
+  );
 }
