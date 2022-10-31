e44b59a11f860958ab804266bb93c6c82af358b8
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-10-30 22:03:54 -0300

    styled root

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,13 +1,20 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import Navbar from "../../components/Navbar";
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 export default function Root() {
   return (
     <>
       <Navbar nav />
-      <main>
+      <Main>
         <Outlet />
-      </main>
+      </Main>
     </>
   );
 }
