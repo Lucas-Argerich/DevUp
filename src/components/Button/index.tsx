de01af991fc9cf07bd604096ba998a844b44d76d
Lucas Argerich <lucasargerichcoder@gmail.com>
2022-11-05 20:18:01 -0300

    Button styling

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,35 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  padding: 10px 40px;
+  background: var(--color-gray);
+  border: none;
+  border-radius: 100px;
+  font-weight: 600;
+  display: flex;
+  justify-content: space-between;
+  min-width: 300px;
+  align-items: center;
+  font-size: 1rem;
+
+  svg {
+    width: 24px;
+  }
+
+  span {
+    margin: 0 auto;
+  }
+`;
+
 type Props = {
-  children?: string | JSX.Element;
+  children: JSX.Element | JSX.Element[];
   onClick: () => void;
 };
 
 export default function Button({ children, onClick }: Props) {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <StyledButton onClick={onClick}>
+      {children}
+    </StyledButton>
+  );
 }
